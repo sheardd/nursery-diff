b25be2617a89901c86165e1cf3a75b276d8e98ec
David Sheard <david.sheard@hotmail.com>
2018-10-03 17:35:15 +0100

    add configs dir, first config as a module, and our primary test script as a module

---
+++ configs/itruns.js
@@ -0,0 +1,14 @@
+const user = "projectnursery";
+const pass = "pnthepassword";
+
+module.exports = {
+	credentials : {
+		username: user,
+		password: pass
+	},
+	paths : [
+	{"name": "home",
+	"endpoint": "/blog"}
+	],
+	url : "https://itruns.co.uk"
+};
---
+++ src/test.js
@@ -0,0 +1,30 @@
+const Differencify = require('differencify');
+const differencify = new Differencify();
+
+async function testPath(url, path, credentials = false) {
+	const target = differencify.init({ chain: false });
+	await target.launch();
+	const page = await target.newPage();
+	await page.setViewport({ width: 1600, height: 1200 });
+	if (credentials) {
+		console.log("credentials: " + credentials);
+		await page.authenticate(credentials);
+	}
+	await page.goto(url + path.endpoint);
+	await page.waitFor(1000);
+	const image = await page.screenshot();
+	await target.toMatchSnapshot(image)
+	await page.close();
+	await target.close();
+}
+
+module.exports = {
+	testAllPaths : async function(url, paths, credentials){
+		for (var i = 0; i < paths.length; i++){
+			var path = paths[i];
+			console.log("Capturing: " + path.name);
+			await testPath(url, path, credentials);
+		}
+		console.log("Tests Complete");
+	}
+};
