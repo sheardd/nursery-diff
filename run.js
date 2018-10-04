1dbbc2c761305ae0dac7a9de3b4717ab5728b066
David Sheard <david.sheard@hotmail.com>
2018-10-04 12:55:05 +0100

    Change run.js to accept a global variable url, modify test.js to test the same page across multiple widths

--- run.js
+++ run.js
@@ -1,6 +1,5 @@
 const test= require("./src/test.js");
-const config = require("./configs/itruns.js");
+const config = require("./configs/config.js");
 
-console.log("Beginning captures on " + config.url);
-
-test.testAllPaths(config.url, config.paths, config.credentials);
+console.log("Beginning captures on " + process.env.url);
+test.testAllPaths(process.env.url, config.paths, config.credentials, config.widths);
--- src/test.js
+++ src/test.js
@@ -1,11 +1,28 @@
 const Differencify = require('differencify');
 const differencify = new Differencify();
 
-async function testPath(url, path, credentials = false) {
-	const browser = differencify.init({ chain: false });
-	await browser.launch();
+async function testPath(url, path, credentials = false, widths = false) {
+	const browser = differencify.init({ testName: path.name,
+									chain: false });
+	// var width;
+	if (widths) {
+		for (var i = 0; i < widths.length; i++) {
+			await capture(browser, url, path, width = widths[i], credentials);
+		}
+	} else {
+		await capture(browser, url, path, width = 1600, credentials);
+	}
+	
+	await browser.close();
+
+	return;
+}
+
+async function capture(browser, url, path, width, credentials = false){
+	console.log("Capturing at " + width + "px");
 	const page = await browser.newPage();
-	await page.setViewport({ width: 1600, height: 1200 });
+	const viewport = Object.assign(page.viewport(), {width: width});
+	await page.setViewport(viewport);
 	if (credentials) {
 		await page.authenticate(credentials);
 	}
@@ -16,18 +33,17 @@ async function testPath(url, path, credentials = false) {
 		console.log(results);
 	});
 	await page.close();
-	await browser.close();
-
-	return;
 }
 
 module.exports = {
-	testAllPaths : async function(url, paths, credentials){
+	testAllPaths : async function(url, paths, credentials, widths = false){
+		await differencify.launchBrowser();
 		for (var i = 0; i < paths.length; i++){
 			var path = paths[i];
 			console.log("Capturing: " + path.name);
-			await testPath(url, path, credentials);
+			await testPath(url, path, credentials, widths);
 		}
+		await differencify.cleanup();
 		console.log("Tests Complete");
 		return;
 	}
